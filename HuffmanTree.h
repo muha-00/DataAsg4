d3f25e539e5179cc124446e123a6b8abaf17cbd0
Muhammad <muhammad87711@gmail.com>
2025-03-27 20:22:12 -0400

    defined the node structure, added constructors

--- HuffmanTree.h
+++ HuffmanTree.h
@@ -5,9 +5,27 @@
 #ifndef HUFFMANTREE_H
 #define HUFFMANTREE_H
 
+#include <string>
+#include <map>
+#include <queue>
 
 
-class HuffmanTree {
+// HuffmanNode structure to store character data and tree linkages
+struct HuffmanTree {
+    char character;
+    int frequency;
+    HuffmanNode* left;
+    HuffmanNode* right;
+
+    HuffmanNode(char ch, int freq)
+        : character(ch), frequency(freq), left(nullptr), right(nullptr) {}
+
+    HuffmanNode(int freq)
+        : character('\0'), frequency(freq), left(nullptr), right(nullptr) {}
+};
+
+
+
 
 };
 
